5ad059a6bc5a8d2093aeb3512905bd581752d59b
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2024-06-22 17:30:33 +0530

    binary adder using nerual network

--- main.c
+++ main.c
@@ -3,40 +3,57 @@
 
 #include "mlp.h"
 
-// A ^ B = A NAND B) (A NDNA)
-
 int main(void)
 {
-	int layers[] = {2, 2, 1};
+	int layers[] = {3, 3, 2};
 
 	mlp_t model = mlp_create(layers, sizeof layers / sizeof layers[0], layers[0]);
 	mlp_t dmod  = mlp_copy_arch(&model);
 
 	traning_data_t td[] = {
-		{(float []){0, 0}, (float []){1}},
-		{(float []){0, 1}, (float []){0.7}},
-		{(float []){1, 0}, (float []){0}},
-		{(float []){1, 1}, (float []){1}},
+		{(float []){0, 0, 0}, (float []){0, 0}},
+		{(float []){0, 0, 1}, (float []){0, 1}},
+		{(float []){0, 1, 0}, (float []){0, 1}},
+		{(float []){0, 1, 1}, (float []){1, 0}},
+		{(float []){1, 0, 0}, (float []){0, 1}},
+		{(float []){1, 0, 1}, (float []){1, 0}},
+		{(float []){1, 1, 0}, (float []){1, 0}},
+		{(float []){1, 1, 1}, (float []){1, 1}},
 	};
 
-	float dc_da = 0;
-	float output = 0;
+	float dc_da[2] = {0};
+	float output[2] = {0};
 	float cost = 0;
 
-	for(int k = 0; k < 1000 * 100; k++)
+	int epochs = 1000 * 200;
+	for(int k = 0; k < epochs; k++)
 	{
-		cost = mlp_train(&model, &dmod, td, sizeof td / sizeof td[0], &output, &dc_da, 0.15);
+		cost = mlp_train(&model, &dmod, td, sizeof td / sizeof td[0], output, dc_da, 0.01);
+
+		if(k % (epochs / 10) == 0)
+			printf("cost = %f\n", cost);
 	}
 
+	puts("");
 	mlp_print(&model);
 
-	printf("cost = %f\n", cost);
 
 	for(int i = 0; i < sizeof td / sizeof td[0]; i++)
 	{
-		mlp_forward(&model, td[i].input, &output);
-		printf("%d | %f * %f = %f \n", i + 1, td[i].input[0], td[i].input[1], output);
+		mlp_forward(&model, td[i].input, output);
+
+		printf("%d | %f %f %f = %f %f\n",
+				i,
+				td[i].input[0],
+				td[i].input[1],
+				td[i].input[2],
+				output[0], output[1]);
 	}
+
+	float input[] = {1, 0, 1};
+	mlp_forward(&model, input, output);
+
+	puts("");
 
 	return 0;
 }
